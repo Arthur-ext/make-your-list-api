769929f66773fae917348751fbf5956363b4c633
Arthur S <arthur.ext@gmail.com>
2022-07-18 01:45:55 -0300

    receive application and set controller to Handler

--- api/router/router.go
+++ api/router/router.go
@@ -4,22 +4,25 @@ import (
 	"time"
 
 	"wedding_gifts/api/handler"
+	"wedding_gifts/internal/app"
+	apiMiddleware "wedding_gifts/api/middleware"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func InitRouter() *chi.Mux {
+func InitRouter(a app.APP) *chi.Mux {
 	router := chi.NewRouter()
-
+	
 	router.Use(
 		middleware.Logger,
 		middleware.Recoverer,
 		middleware.Timeout(10 * time.Second),
+		apiMiddleware.SetHeaderContentType("application/json"),
 	)
 
 	router.Route("/api", func(r chi.Router) {
-		r.Mount("/wedding-gifts", handler.Routes())
+		r.Mount("/wedding-gifts", handler.NewGiftHandler(a.Gifts).Routes())
 	})
 
 	return router
