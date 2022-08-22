ad3bf396f61f3e7944c6b60c2b8b3530826bae2a
Arthur S <arthur.ext@gmail.com>
2022-08-21 23:10:38 -0300

    now using cloud mongodb, with atlas :

--- internal/database/mongo.go
+++ internal/database/mongo.go
@@ -2,11 +2,16 @@ package database
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"github.com/joho/godotenv"
 )
 
 type MongoClient struct {
@@ -14,8 +19,15 @@ type MongoClient struct {
 }
 
 func NewMongoClient() (*MongoClient, error) {
-	uri := "mongodb://root:rootpswd@mongo:27017"
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("some error occured. Err: %s", err)
+	}
+	mongo_pass := os.Getenv("MONGO_PSWD")
 
+	uri := fmt.Sprintf("mongodb+srv://wedding-gifts:%s@cluster0.fle2wmc.mongodb.net/?retryWrites=true&w=majority", mongo_pass)
+	print(uri)
+	// uri := "mongodb://root:rootpswd@mongo:27017"
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
