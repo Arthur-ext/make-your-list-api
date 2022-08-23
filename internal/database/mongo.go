799a7d1e77ec3c1b3dfdebeded41a0ff41a1e02b
Arthur S <arthur.ext@gmail.com>
2022-08-22 22:08:13 -0300

    remove print

--- internal/database/mongo.go
+++ internal/database/mongo.go
@@ -28,8 +28,7 @@ func NewMongoClient() (*MongoClient, error) {
 	mongo_pass := os.Getenv("MONGO_PSWD")
 
 	uri := fmt.Sprintf("mongodb+srv://wedding-gifts:%s@cluster0.fle2wmc.mongodb.net/?retryWrites=true&w=majority", mongo_pass)
-	print(uri)
-	// uri := "mongodb://root:rootpswd@mongo:27017"
+	
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
